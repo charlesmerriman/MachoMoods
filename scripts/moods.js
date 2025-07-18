f672d83613485133b564f8f145aaa24bb6416c36
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-18 11:59:12 -0500

    working on CSS

--- scripts/moods.js
+++ scripts/moods.js
@@ -4,16 +4,23 @@ export const generateHTML = (moods) => {
     let moodsHTML = ""
     for (const mood of moods) {
         moodsHTML += `
-        <article>
-            <h1>${mood.name}</h1>
-            <img src="${mood.imageUrl}">
-            <p>Macho Advice:</p>`
+        <article class="flex-container">
+            <div class="flex-container articleHeader">
+                <h1>${mood.name}</h1>
+                <img src="${mood.imageUrl}">
+            </div>
+            <div class="flex-container row articleAdvice">
+                <p>Macho Advice:</p>
+                <div class="flex-container">`
             for (const quote of mood.quotes){
                 moodsHTML+= `
                 <p>${quote}</p>
                 `
             }
-        `</article>
+        moodsHTML+= `
+                </div>
+            </div>  
+        </article>
         `
     }
     return moodsHTML
